09b9005fe9ef9d2cc8c77f3336fc271c2d08f5aa
test-git <dholariyarahul@gmail.com>
2019-06-22 14:18:53 +0530

    demo App v1.0

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from flask import Flask, session,render_template, request
 app = Flask(__name__)
 
 # Check for environment variable
-'''
+
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
@@ -20,13 +20,13 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-'''
+
 
 @app.route("/")
 def index():
     return "<h1>Hello, Doco Book</h1>"
     #return render_template("index.html")
-'''
+
 @app.route("/store",methods=['POST'])
 def store():
     return render_template("store.html")
@@ -34,7 +34,7 @@ def store():
 @app.route("/registration",methods=['GET', 'POST'])
 def registration():
     return render_template("registration.html")
-'''
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
