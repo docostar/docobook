be4dde1ad304112e8a7775f9e2df165303a3248c
test-git <dholariyarahul@gmail.com>
2019-06-22 14:42:27 +0530

    demo App v1.0

--- application.py
+++ application.py
@@ -1,13 +1,14 @@
 import os
 
 from flask import Flask, session,render_template, request
-#from flask_session import Session
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
 # Check for environment variable
+
 
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
